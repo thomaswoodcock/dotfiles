3ec9a4ff2ba91ff1ad569bf4a072434aec2d2fab
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-03-29 16:00:26 +0100

    Installs Scoop, Git and VS Code

--- Install.ps1
+++ Install.ps1
@@ -2,7 +2,29 @@
 
 . .\Helpers.ps1
 
+# Install Scoop
+Write-Host "Installing Scoop..."
+
+if (-not(Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+# Add Scoop buckets
+Write-Host "Adding Scoop buckets..."
+scoop bucket add extras
+
+# Install applications using Scoop
+Write-Host "Installing applications using Scoop..."
+scoop install git vscode
+
+# Update applications
+Write-Host "Updating applications using Scoop..."
+scoop update *
+
 # Sync Visual Studio Code extensions
+Write-Host "Syncing Visual Studio Code extensions..."
+
 $extensions = @(
     "dbaeumer.vscode-eslint",
     "eamodio.gitlens",
@@ -12,16 +34,16 @@ $extensions = @(
     "zhuangtongfa.material-theme"
 )
 
-Write-Host "Syncing Visual Studio Code extensions..." -ForegroundColor Yellow
 SyncVsCodeExtensions $extensions
 
 # Create symbolic links
+Write-Host "Creating symbolic links..."
+
 $symbolicLinks = @(
     ("${env:USERPROFILE}\.gitconfig",".\.gitconfig"),
     ("${env:APPDATA}\Code\User\settings.json",".\vscode.settings.json")
 )
 
-Write-Host "Creating symbolic links..." -ForegroundColor Yellow
 CreateSymbolicLinks $symbolicLinks
 
 Write-Host "Installation complete." -ForegroundColor Green
